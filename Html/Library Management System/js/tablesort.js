cd61691104d284f5c1b64a9381d9e163e0688f04
KalyaniPatil23 <kalyanipatil2307@gmail.com>
2021-07-06 21:36:02 +0530

    Modified

--- Html/Library Management System/js/tablesort.js
+++ Html/Library Management System/js/tablesort.js
@@ -1,17 +1,24 @@
 
 function sortTableByColumn(column){
     var table = document.getElementById("bookTable");
+
+// display value property of select list (from selected option)
+console.log( `${column + 1}` );
     //var rows=table.rows;
     const tBody = table.tBodies[0];
     console.log(tBody.innerHTML);
     const rows=Array.from(tBody.querySelectorAll("tr"));
+    console.log(tBody.querySelectorAll("tr").innerHTML);
     const sortedRows = rows.sort((a, b) => {
+        console.log(a.querySelector(`td:nth-child(2)`));
         const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+        console.log(aColText);
         const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+        console.log(bColText);
 
-        return aColText > bColText ? (1 * 1) : (-1 * 1);
+        return aColText > bColText ? 1 : -1 ;
     });
-    console.log(sortedRows);
+    console.log(sortedRows.values);
     // Remove all existing TRs from the table
     while (tBody.firstChild) {
         tBody.removeChild(tBody.firstChild);
@@ -19,5 +26,5 @@ function sortTableByColumn(column){
     // Re-add the newly sorted rows
     tBody.append(...sortedRows);
 
-    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    //table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
 }
